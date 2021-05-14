5179a4b02718033b8205aeb1724e83ddf93833c1
Camily-c <laceyj2.laceyj2@gmail.com>
2021-05-14 13:41:20 -0300

    Fiz a verificação ter mais de um produto e adicionar a quantidade

--- src/Components/Carrinho/Carrinho.js
+++ src/Components/Carrinho/Carrinho.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import Produtos from '../Produtos/Produtos';
 
 const ContainerCarrinho = styled.div`
     /* width: 20vw; */
@@ -10,6 +11,27 @@ const ContainerCarrinho = styled.div`
 
 
 export default class Carrinho extends Component {
+    
+    addProduto(produtoId){
+        var novoCarrinho = this.state.Carrinho;
+        let jaExiste = false;
+
+        for (let i = 0; novoCarrinho.length; i++){
+            if(produtoId === novoCarrinho[i].id){
+                jaExiste = true;
+                novoCarrinho[i].Quantidade += 1;
+            }
+        }
+        if (!jaExiste){
+            novoCarrinho.push(
+                this.state.produtos.find((produto) => {
+                    return produto.id === produtoId ? produto : false;
+                })
+            )
+        }
+    }
+    
+    
     render() {
 
         return (
