36ddebc935ca548c510fbab54be10342ad4288d8
GuilhermeSoutello <guisoutello@gmail.com>
2021-05-16 21:35:00 -0300

    headerzinho

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 
+
 const Container = styled.div`
 `
+const ContainerLogo = styled.div`
+
+`
+
+const SessaoLoja = styled.div`
+`
+
 
 export default class Header extends React.Component {
   render() {
     return (
         <Container>
-          Header Aqui
+          <ContainerLogo>
+          <imagem src={null}/>
+          </ContainerLogo>
+        <SessaoLoja>
+            <a href="#Novidades">Novidades</a>
+            <a href="#Marcas">Marcas</a>
+            <a href="#Outlet">Outlet</a>
+        </SessaoLoja>
+
         </Container>
     );
   }
