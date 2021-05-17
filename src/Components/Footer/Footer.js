24eada846fc0d6d76152acdfd926d95b7a3926de
Bianca Leonardo <biancaleonardo@pop-os.localdomain>
2021-05-16 21:14:59 -0300

    estilizando footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,59 +1,54 @@
 import React from "react";
 import styled from "styled-components";
-import { Container } from "../../AppStyle";
+import IconeCell from './img/cell-white.svg'
+import IconeEmail from './img/email-white.svg'
+import IconeLocale from './img/locale-white.svg'
+import {ContainerFooter, ContainerSAC, ContainerContato} from './FooterStyle'
 
-const ContainerFooter = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 1fr;
-  justify-items: center;
-`;
-const ContainerAboutUs = styled.div``;
-const ContainerSAC = styled.div``;
-const ContainerContato = styled.div``;
-const Copy = styled.div`
 
-`
 
 export default class Footer extends React.Component {
   render() {
     return (
       <ContainerFooter>
-        <ContainerAboutUs>
+        <div>
           <h3>Sobre Nós</h3>
           <p>Loja especializada em produtos de outro Mundo...</p>
-        </ContainerAboutUs>
+        </div>
         <ContainerSAC>
           <h3>Recursos</h3>
-          <ul>
-            <li>
-              <a href="#">Faça sua Conta</a>
-            </li>
-            <li>
-              <a href="#">FAQ</a>
-            </li>
-            <li>
-              <a href="#">Nossos Produtos</a>
-            </li>
-            <li>
-              <a href="#">Conheça a SpaceStle</a>
-            </li>
-          </ul>
+          <div>
+            <ul>
+              <li>
+                <a href="#">Faça sua Conta</a>
+              </li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">Nossos Produtos</a>
+              </li>
+              <li>
+                <a href="#">Conheça a SpaceStle</a>
+              </li>
+            </ul>
+          </div>
+
         </ContainerSAC>
         <ContainerContato>
           <h3>Contato</h3>
           <ul>
             <li>
-              <span>1123, Campo de Marte, São Paulo, SP, Brasil.</span>
+              <img src={IconeLocale} alt={'icone localização'} /><span>1123, Campo de Marte, São Paulo, SP, Brasil.</span>
             </li>
             <li>
               <p>
-                <a href="+55 912345678">+55 912345678</a>
+                <img src={IconeCell} alt={'icone contato'} /><a href="+55 912345678">+55 912345678</a>
               </p>
             </li>
             <li>
               <p>
-                <a href="Prox@Cetauri@vialactea.com">
+                <img src={IconeEmail} alt={'icone email'} /><a href="Prox@Cetauri@vialactea.com">
                   ProxCentauri@Vialactea.com
                 </a>
               </p>
